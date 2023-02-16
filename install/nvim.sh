e7eb5345265a71860ded26257d88f82e8df05c56
Mike Kot <to@myrrc.dev>
2023-02-16 17:51:17 +0300

    add nvim ru spell load; add kitty term config

--- install/nvim.sh
+++ install/nvim.sh
@@ -2,4 +2,4 @@ wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 sudo dpkg -i nvim-linux64.deb
 rm nvim-linux64.deb
 git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless +PackerSync +qa
+wget http://ftp.vim.org/pub/vim/runtime/spell/ru.utf8.spl ~/.configs/nvim/spell/
