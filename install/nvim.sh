9453c8526df2d18c01c227ce73935481ad67f534
Mike Kot <to@myrrc.dev>
2023-03-05 03:10:58 +0300

    upd install scripts

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,7 +1,4 @@
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-sudo dpkg -i nvim-linux64.deb
-rm nvim-linux64.deb
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb -O nvim.deb
+sudo dpkg -i nvim.deb; rm nvim.deb
 git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-mkdir ~/.config/nvim/spell
-cd ~/.config/nvim/spell
-wget http://ftp.vim.org/pub/vim/runtime/spell/ru.utf-8.spl
+mkdir ~/.config/nvim/spell; wget http://ftp.vim.org/pub/vim/runtime/spell/ru.utf-8.spl -P ~/.config/nvim/spell
